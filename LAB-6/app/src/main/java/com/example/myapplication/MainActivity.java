b306c369210e2d9bd6a672fcb059990582524836
Sunit Pal <111626550+Ezio7777@users.noreply.github.com>
2023-05-15 22:28:35 +0530

    LAB-6

---
+++ LAB-6/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    boolean isEgg=true;
+    public void change(View view){
+        ImageView iv=findViewById(R.id.imageView);
+        if(isEgg){
+            iv.setImageResource(R.drawable.images);
+            isEgg=false;
+        }else{
+            iv.setImageResource(R.drawable.download);
+            isEgg=true;
+        }
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
